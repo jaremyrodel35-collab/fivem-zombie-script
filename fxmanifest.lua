fcf3a22f40e539fa29102ac746e3b76a5333a3d6
jaremyrodel35-collab <jaremyrodel35@gmail.com>
2026-09-09 17:28:57 +0200

    Update fxmanifest with loading screen UI and new files

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -2,16 +2,25 @@ fx_version 'cerulean'
 game 'gta5'
 
 author 'Zombie Script'
-description 'Complete FiveM Zombie Script with animations and sounds'
-version '1.0.0'
+description 'Complete FiveM Zombie Script with animations, sounds, loading screen and music control'
+version '2.0.0'
+
+ui_page 'html/index.html'
 
 scripts {
   'client/client.lua',
+  'client/loading.lua',
   'server/server.lua'
 }
 
 shared_scripts {
   'shared/config.lua'
+}
+
+files {
+  'html/index.html',
+  'html/style.css',
+  'html/script.js'
 }
 
 dependencies {
